0f5ec63d0f47e58f13dac25c938a48a2022ce068
Victória Lamarão <54338477+VictoriaLamarao@users.noreply.github.com>
2022-08-10 12:45:38 -0300

    Add files via upload

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,60 +1,128 @@
 const produtos = [
-    {
-        nome: 'Banana',
-        preco: 4,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: "./src/img/banana.png"
+  {
+    id: 1,
+    nome: 'Banana',
+    preco: 4.00,
+    secao: 'Hortifruti',
+    categoria: 'fruta',
+    img: "./src/img/banana.png",
+    promocao: true,
+    precoPromocao: '1.00',
+    componentes: [
+      'Potássio',
+      'Vitamina B6',
+      'Vitamina C',
+      'Folatos'
+    ]
+  },
+  {
+    id: 2,
+    nome: 'Morango',
+    preco: 7.00,
+    secao: 'Hortifruti',
+    categoria: 'fruta',
+    img: './src/img/morango.png',
+    promocao: false,
+    precoPromocao: '0',
+    componentes: [
+      'Fibras',
+      'Vitamina C',
+      'Cálcio',
+      'Ferro'
+    ]
+  },
+  {
+    id: 3,
+    nome: 'Maçã',
+    preco: 4.00,
+    secao: 'Hortifruti',
+    categoria: 'fruta',
+    img: './src/img/maça.png',
+    promocao: true,
+    precoPromocao: '1.50',
+    componentes: [
+      'Potássio',
+      'Vitamina A',
+      'Vitamina E',
+      'Vitamina C'
+    ]
+  },
+  {
+    id: 4,
+    nome: 'Pão',
+    preco: 5.00,
+    secao: 'Panificadora',
+    categoria: 'Pães',
+    img: './src/img/pao.png',
+    promocao: true,
+    precoPromocao: '2.50',
+    componentes: [
+      'Cálcio',
+      'Sódio',
+      'Fibra Alimentar',
+      'Proteínas'
+    ]
+  },
+  {
+    id: 5,
+    nome: 'Leite',
+    preco: 7.00,
+    secao: 'Laticinio',
+    categoria: 'Leite',
+    img: './src/img/leite.png',
+    promocao: false,
+    precoPromocao: '1.00',
+    componentes: [
+      'Carboidratos',
+      'Proteínas',
+      'Gorduras totais'
+    ]
+  },{
+    id: 6,
+    nome: 'Torta de Maçã',
+    preco: 4.00,
+    secao: 'Panificadora',
+    categoria: 'doces',
+    img: './src/img/torta de maçã.jpg',
+    promocao: false,
+    precoPromocao: '3.00',
+    componentes: [
+      'Carboidratos',
+      'Proteínas',
+      'Gorduras totais'
+    ]
+  },
+  {
+      id: 7,
+      nome: 'Bolo',
+      preco: 10.00,
+      secao: 'Panificadora',
+      categoria: 'doces',
+      img: './src/img/bolo.jpeg',
+      promocao: true,
+      precoPromocao: '5.00',
+      componentes: [
+        'Carboidratos',
+        'Proteínas',
+        'Gorduras totais'
+      ]
     },
     {
-        nome: 'Morango',
-        preco: 7,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: './src/img/morango.png'
-    },
-    {
-        nome: 'Maçã',
-        preco: 4,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: './src/img/maça.png'
-    },
-    {
-        nome: 'Pão',
-        preco: 5,
-        secao: 'Panificadora',
-        categoria: 'Pães',
-        img: './src/img/pao.png'
-    },
-    {
-        nome: 'Leite',
-        preco: 7,
-        secao: 'Laticínio',
-        categoria: 'Leite',
-        img: './src/img/leite.png'
-    },
-    {
-        nome: 'Iogurte de Morango',
-        preco: 3,
-        secao: 'Laticínio',
-        categoria: 'Leite',
-        img: './src/img/yogurte.jpeg'
-    },
-    {
-        nome: 'Torta de Maçã',
-        preco: 4,
-        secao: 'Panificadora',
-        categoria: 'doces',
-        img: './src/img/torta de maçã.jpg'
-    },
-    {
-        nome: 'Bolo',
-        preco: 10,
-        secao: 'Panificadora',
-        categoria: 'doces',
-        img: './src/img/bolo.jpeg'
-    } 
-   
-    
+      id: 8,
+      nome: 'Iogurte de Morango',
+      preco: 3.00,
+      secao: 'Laticinio',
+      categoria: 'Leite',
+      img: './src/img/yogurte.jpeg',
+      promocao: false,
+      precoPromocao: '1.00',
+      componentes: [
+        'Carboidratos',
+        'Proteínas',
+        'Gorduras totais'
+      ]
+    }
 ]
+ 
+  
+
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,20 @@
 let button = document.querySelector(
   "estiloGeralBotoes estiloGeralBotoes--filter"
 );
-
+let valor = document.querySelector("span");
+let ulCarrinho = document.querySelector(".produtosCarrinho");
 let ulP = document.querySelector(".listaProdutos");
+let divBotoes = document.querySelector("#botoesContainer");
+let button2 = document.querySelector(
+  "estiloGeralBotoes estiloGeralBotoes--botaoBuscaPorNome"
+);
+let div = document.querySelector(".containerBuscaPorNome");
+let searchInput = document.querySelector("input");
+let sectionVitrine = document.querySelector(".containerVitrine");
+let sectionCarrinho = document.querySelector(".carrinho");
+let divCar = document.querySelector(".notProdutos");
+let arrReceber = [];
+//let buttonRemove = document.querySelector(".botao-remover")
 
 // função criar lista de produtos
 function criarListas(objeto) {
@@ -10,21 +22,42 @@ function criarListas(objeto) {
   let secao = objeto.secao;
   let price = objeto.preco;
   let category = objeto.category;
+  let id = objeto.id;
 
   let li = document.createElement("li");
+  li.classList.add("vitrine");
   let image = document.createElement("img");
   let h3 = document.createElement("h3");
   let span = document.createElement("p");
   let p = document.createElement("span");
+  let button = document.createElement("button");
+  button.classList.add("button-comprar");
+  button.setAttribute("id", id);
+  button.addEventListener("click", addCar);
 
   image.src = objeto.img;
   h3.innerText = titulo;
   span.innerText = secao;
-  p.innerText = `R$ ${price},00`;
+  button.innerText = "Comprar";
+  p.innerText = `R$ ${price}.00`;
 
-  li.append(image, h3, span, p);
+  li.append(image, h3, span, criarOlLista2(objeto), p, button);
 
   return li;
+}
+
+function criarOlLista2(objeto) {
+  let ol = document.createElement("ol");
+  ol.classList.add("classOl")
+  let componentes = objeto.componentes;
+
+  componentes.forEach((elemento) => {
+    let liS = document.createElement("li");
+    liS.innerText = elemento;
+    ol.append(liS);
+  });
+
+  return ol;
 }
 
 //função para gerar as listas na pagina
@@ -39,9 +72,6 @@ function gerador(arr) {
 
 gerador(produtos);
 
-// usando eventlistener
-//let section = document.querySelector(".filtersContainer");
-let divBotoes = document.querySelector("#botoesContainer");
 divBotoes.addEventListener("click", filtroDeProdutos);
 
 //função para filtrar os produtos quando clica nos botões
@@ -79,18 +109,12 @@ function filtroDeProdutos(event) {
 }
 
 // barra de pesquisa
-let button2 = document.querySelector(
-  "estiloGeralBotoes estiloGeralBotoes--botaoBuscaPorNome"
-);
-let div = document.querySelector(".containerBuscaPorNome");
-let searchInput = document.querySelector("input");
-//let newArr = []
+
 addEventListener("keyup", pesquisar);
-//const value = e.target.value
 
 function pesquisar() {
   ulP.innerHTML = " ";
-  let newArr = []; 
+  let newArr = [];
   let texto = searchInput.value.toLowerCase();
   newArr = produtos.filter((x) => {
     if (x.nome.toLowerCase().includes(texto)) {
@@ -99,18 +123,92 @@ function pesquisar() {
   });
 
   gerador(newArr);
-  somaProdutos(newArr)
-  
+  somaProdutos(newArr);
 }
-
-
-let valor = document.querySelector("span");
 
 function somaProdutos(arr) {
   let resultado = 0;
   for (let i = 0; i < arr.length; i++) {
     resultado += arr[i].preco;
   }
-  valor.innerText = `R$ ${resultado},00`;
+  valor.innerText = `R$${resultado}.00`;
 }
 somaProdutos(produtos);
+
+function criarLiCarrinho(objeto, index) {
+  let nomeTitulo = objeto.nome;
+  let price = objeto.preco;
+  let section = objeto.secao;
+  let category = objeto.categoria;
+  let id = objeto.id;
+
+  let image = document.createElement("img");
+  let div = document.createElement("div");
+  let pTitulo = document.createElement("p");
+  let pPrice = document.createElement("p");
+  let spanSection = document.createElement("span");
+  var button2 = document.createElement("button");
+  button2.classList.add("botao-remover");
+  let li = document.createElement("li");
+  li.classList.add("lista-de-produtos");
+  button2.addEventListener("click", removeCar);
+
+  image.src = objeto.img;
+  pTitulo.innerText = nomeTitulo;
+  spanSection.innerText = section;
+  pPrice.innerText = `R$ ${price}.00`;
+  button2.innerText = "Remover";
+  li.setAttribute("id", id);
+
+  div.append(pTitulo, spanSection, pPrice);
+  li.append(image, div, button2);
+
+  return li;
+}
+
+//geradorCarrinho(produtos);
+function geradorCarrinho(arrReceber) {
+  //console.log(`você chamou a função gerador carrinho ${arrReceber}`);
+  for (let i = 0; i < arrReceber.length; i++) {
+    let objeto = arrReceber[i];
+    let objeto2 = criarLiCarrinho(objeto);
+    ulCarrinho.append(objeto2);
+  }
+
+  return ulCarrinho;
+}
+
+function addCar(event) {
+  
+  let elemento = event.target;
+
+  // let verificarPai = elemento.closest("li");
+  // let consultarIdPai = verificarPai.id;
+  //section.innerHTML = "";
+  ulCarrinho.innerHTML = "";
+  if (elemento.tagName == "BUTTON") {
+    for (let i = 0; i < produtos.length; i++) {
+      if (elemento.id == produtos[i].id) {
+        arrReceber.push(produtos[i]);
+      }
+    }
+  }
+
+  geradorCarrinho(arrReceber);
+}
+
+function removeCar(event) {
+  let elemento = event.target;
+  let verificar = elemento.closest("li");
+  let consultarVerificar = verificar.id;
+  ulCarrinho.innerHTML = "";
+
+  if (elemento.tagName == "BUTTON") {
+    for (let i = 0; i < arrReceber.length; i++) {
+      if (consultarVerificar == arrReceber[i].id) {
+        arrReceber.splice(arrReceber.indexOf(arrReceber[i]), 1);
+        geradorCarrinho(arrReceber);
+      }
+    }
+  }
+}
